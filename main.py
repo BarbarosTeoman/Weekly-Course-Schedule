76f53fddc085959d7501d30e5686ba2ea874ce28
BarbarosTeoman <91433408+BarbarosTeoman@users.noreply.github.com>
2023-04-18 23:48:07 +0000

    Final

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from itertools import islice
 from tkinter import *
 
-
 def nth_index(iterable, value, n):
   matches = (idx for idx, val in enumerate(iterable) if val == value)
   return next(islice(matches, n - 1, n), None)
@@ -20,7 +19,6 @@ class Course:
     self.end_time = end_time
     self.__class__.instances.append(self)
 
-
 class Schedule:
   days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
   programme = [[
@@ -28,26 +26,26 @@ class Schedule:
     "12.30 - 13.30", "13.30 - 14.30", "14.30 - 15.30", "15.30 - 16.30",
     "16.30 - 17.30"
   ],
-               [
-                 " 8.30 -  9.30", " 9.30 - 10.30", "10.30 - 11.30",
-                 "11.30 - 12.30", "12.30 - 13.30", "13.30 - 14.30",
-                 "14.30 - 15.30", "15.30 - 16.30", "16.30 - 17.30"
-               ],
-               [
-                 " 8.30 -  9.30", " 9.30 - 10.30", "10.30 - 11.30",
-                 "11.30 - 12.30", "12.30 - 13.30", "13.30 - 14.30",
-                 "14.30 - 15.30", "15.30 - 16.30", "16.30 - 17.30"
-               ],
-               [
-                 " 8.30 -  9.30", " 9.30 - 10.30", "10.30 - 11.30",
-                 "11.30 - 12.30", "12.30 - 13.30", "13.30 - 14.30",
-                 "14.30 - 15.30", "15.30 - 16.30", "16.30 - 17.30"
-               ],
-               [
-                 " 8.30 -  9.30", " 9.30 - 10.30", "10.30 - 11.30",
-                 "11.30 - 12.30", "12.30 - 13.30", "13.30 - 14.30",
-                 "14.30 - 15.30", "15.30 - 16.30", "16.30 - 17.30"
-               ]]
+  [
+    " 8.30 -  9.30", " 9.30 - 10.30", "10.30 - 11.30",
+    "11.30 - 12.30", "12.30 - 13.30", "13.30 - 14.30",
+    "14.30 - 15.30", "15.30 - 16.30", "16.30 - 17.30"
+  ],
+  [
+    " 8.30 -  9.30", " 9.30 - 10.30", "10.30 - 11.30",
+    "11.30 - 12.30", "12.30 - 13.30", "13.30 - 14.30",
+    "14.30 - 15.30", "15.30 - 16.30", "16.30 - 17.30"
+  ],
+  [
+    " 8.30 -  9.30", " 9.30 - 10.30", "10.30 - 11.30",
+    "11.30 - 12.30", "12.30 - 13.30", "13.30 - 14.30",
+    "14.30 - 15.30", "15.30 - 16.30", "16.30 - 17.30"
+  ],
+  [
+    " 8.30 -  9.30", " 9.30 - 10.30", "10.30 - 11.30",
+    "11.30 - 12.30", "12.30 - 13.30", "13.30 - 14.30",
+    "14.30 - 15.30", "15.30 - 16.30", "16.30 - 17.30"
+  ]]
 
   def __init__(self):
     self.courses = []
@@ -61,22 +59,29 @@ class Schedule:
     starts = [k.start_time for k in Course.instances]
     ends = [k.end_time for k in Course.instances]
     collapsingDays = []
+    
     for day in set(allDays):
       allHours = []
       count = allDays.count(day)
+      
       if count == 1:
         continue
       else:
         allHours = []
         collapsingDayIndex = [x for x, n in enumerate(allDays) if n == day]
+        
         for i in collapsingDayIndex:
+          
           for hours in range(starts[i], ends[i]):
+            
             allHours.append(hours)
+            
       if len(allHours) != len(set(allHours)):
         collapsingDays.append(day)
         collusion = True
       else:
         continue
+        
     if collusion:
       collapsingDays.sort()
       a = ", ".join(collapsingDays)
@@ -87,8 +92,7 @@ class Schedule:
 
     for course in self.courses:
       for j in range(course.start_time - 9, course.end_time - 9):
-        Schedule.programme[Schedule.days.index(course.day)][
-          j] += f"      {course.code} {course.name} class from {course.instructor} at {course.classroom}"
+        Schedule.programme[Schedule.days.index(course.day)][j] += f"      {course.code} {course.name} class from {course.instructor} at {course.classroom}"
     for i in range(len(Schedule.days)):
       print(Schedule.days[i], ":")
       for k in Schedule.programme[i]:
